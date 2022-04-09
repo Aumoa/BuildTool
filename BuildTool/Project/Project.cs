631dd7cb5281e5867021048f28de23fce0317405
Aumoa <wkdghcjf1234@naver.com>
2022-04-09 23:35:58 +0900

    PCH 사용 및 반 모듈화

--- BuildTool/Project/Project.cs
+++ BuildTool/Project/Project.cs
@@ -4,6 +4,8 @@ using CodeProjectConfiguration;
 
 using DotNETUtilities;
 
+using System.Text;
+
 namespace BuildTool;
 
 class Project : CompiledModuleCode<ModuleRule>
@@ -13,5 +15,28 @@ class Project : CompiledModuleCode<ModuleRule>
     public Project(FileReference ProjectRuleCode) : base(ProjectRuleCode)
     {
         ProjectDir = RuleCode.GetParent();
+    }
+
+    public string GenerateModuleInclude()
+    {
+        List<string> PublicCodes = new();
+
+        foreach (var IncludePath in CompiledRule.PublicIncludePaths)
+        {
+            PublicCodes.AddRange(Directory.GetFiles(IncludePath, "*.h", SearchOption.AllDirectories).Select(p =>
+            {
+                string Full = Path.GetFullPath(p).Replace('\\', '/');
+                return Full;
+            }));
+        }
+
+        //FileReference ModulePCH = new(Path.Combine(ProjectPath, RuleCode.Name));
+        StringBuilder ModulePCH = new();
+        foreach (var HeaderFile in PublicCodes.Distinct())
+        {
+            ModulePCH.AppendLine($"#include \"{HeaderFile}\"");
+        }
+
+        return ModulePCH.ToString();
     }
 }
